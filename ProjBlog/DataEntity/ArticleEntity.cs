8e9eda760de0af0c9d6ab6c2045676b40db17d01
hoalong317 <hoalong317@gmail.com>
2015-11-24 18:48:38 +0700

    Article Add

---
+++ ProjBlog/DataEntity/ArticleEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using log4net;
+using ProjBlog.DataEntity;
+namespace ProjBlog.DataEntity
+{
+    public class ArticleEntity
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(ArticleEntity));
+        private DbEntity db;
+        public ArticleEntity()
+        { 
+
+        }
+    }
+}
